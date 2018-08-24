1fe93f5f02f4c3d60bd6b8d88597a0f929303e49
Ondrej Hejda <ondrej.hejda@saviotechnics.com>
2018-08-24 09:32:00 +0200

    add ch2 -> servo1, make code simplier

--- main.c
+++ main.c
@@ -12,9 +12,8 @@
 //            |                 |
 //            |     P1.1/UCA0RXD|<--- FS-A8S
 //            |                 |
-//            |             P1.7|---> RED LED (RX ON)
-//            |             P1.6|---> LED GND
-//            |             P2.5|---> GREEN LED (PWM ON)
+//            |             P2.7|---> RED LED (RX ON)
+//            |             P2.6|---> GREEN LED (PWM ON)
 //            |                 |
 //            |       P2.1/TA1.1|---> PWM LEFT
 //            |             P2.2|---> DIR LEFT
@@ -40,13 +39,13 @@
 #define LED_GREEN_OFF() do{P1OUT&=~0x40;}while(0)
 #define LED_GREEN_SWAP() do{P1OUT^=0x40;}while(0)*/
 
-#define LED_INIT() do{P1DIR|=0xC0;P2DIR|=0x20;P1OUT&=~0xC0;P2OUT&=~0x20;}while(0)
-#define LED_RED_ON() do{P2OUT|=0x05;}while(0)
-#define LED_RED_OFF() do{P2OUT&=~0x05;}while(0)
-#define LED_RED_SWAP() do{P2OUT^=0x05;}while(0)
-#define LED_GREEN_ON() do{P1OUT|=0x80;}while(0)
-#define LED_GREEN_OFF() do{P1OUT&=~0x80;}while(0)
-#define LED_GREEN_SWAP() do{P1OUT^=0x80;}while(0)
+#define LED_INIT() do{P2DIR|=0xC0;P2OUT|=0xC0;P2SEL&=~0xC0;P2SEL2&=~0xC0;}while(0)
+#define LED_RED_ON() do{P2OUT|=0x80;}while(0)
+#define LED_RED_OFF() do{P2OUT&=~0x80;}while(0)
+#define LED_RED_SWAP() do{P2OUT^=0x80;}while(0)
+#define LED_GREEN_ON() do{P2OUT|=0x40;}while(0)
+#define LED_GREEN_OFF() do{P2OUT&=~0x40;}while(0)
+#define LED_GREEN_SWAP() do{P2OUT^=0x40;}while(0)
 
 #define CHANNELS 10
 
@@ -55,15 +54,82 @@
 #define CHMIN  0x3E8
 
 #define PWM_THOLD 50
+#define PWM_MAX 500
+#define PWM_LIMIT 500
 
 uint16_t ch[CHANNELS];
 int rxcnt = 0;
+
+void set_servo_1(uint16_t val) {
+    TA0CCR1 = val;
+}
+
+bool set_pwm_1(int16_t val) {
+	bool act = false;
+
+	if (val>0) { // forward
+		P2OUT &= ~0x04;
+		if (val>PWM_THOLD) {
+			TA1CCR1 = val;
+			act = true;
+		} else
+			TA1CCR1 = 0;
+	}
+
+	else { // backward
+		P2OUT |= 0x04;
+		if (val<-PWM_THOLD) {
+			TA1CCR1 = PWM_MAX+val;
+			act = true;
+		} else
+			TA1CCR1 = PWM_MAX+1;
+	}
+	
+	return act;
+}
+
+bool set_pwm_2(int16_t val) {
+	bool act = false;
+	
+	if (val>0) {
+		P2OUT &= ~0x08;
+		if (val>PWM_THOLD) {
+			TA1CCR2 = val;
+			act = true;
+		} else
+			TA1CCR2 = 0;
+	}
+	else {
+		P2OUT |= 0x08;
+		if (val<-PWM_THOLD) {
+			TA1CCR2 = PWM_MAX+val;
+			act = true;
+		} else
+			TA1CCR2 = PWM_MAX+1;
+	}
+
+	return act;
+}
+
+void xy2lr(int16_t x, int16_t y, int16_t *l, int16_t *r) {
+	int16_t ch0i = x-CENTER;
+	int16_t ch1i = y-CENTER;
+	int16_t left  = ch1i + ch0i;
+	int16_t right = ch1i - ch0i;
+	if (left>PWM_LIMIT)   left  =  PWM_LIMIT;
+	if (left<-PWM_LIMIT)  left  = -PWM_LIMIT;
+	if (right>PWM_LIMIT)  right =  PWM_LIMIT;
+	if (right<-PWM_LIMIT) right = -PWM_LIMIT;
+	*l = left;
+	*r = right;
+}
 
 int main(void)
 {
     //WDTCTL = WDTPW + WDTHOLD;        // Stop WDT
     WDTCTL = WDT_MDLY_32;                     // Set Watchdog Timer interval to ~30ms
     IE1 |= WDTIE;                             // Enable WDT interrupt
+	
     if (CALBC1_1MHZ==0xFF) while(1); // If calibration constant erased do not load, trap CPU!!	
     DCOCTL = 0;                      // Select lowest DCOx and MODx settings
     BCSCTL1 = CALBC1_1MHZ;           // Set DCO
@@ -83,77 +149,52 @@ int main(void)
 	P1DIR |= 0x60;
 	P1OUT &= ~0x60;
 	P1SEL |= 0x60;
-	TA0CCTL0 = OUTMOD_1;
     TA0CCR0 = 20000;
-    TA0CCR1 = 1500;
-    TA0CCTL1 = OUTMOD_6;
-    TA0CCR2 = 1500;
-	TA0CCTL2 = CCIE;
+    TA0CCR1 = 0;
+    TA0CCTL1 = OUTMOD_7;
 	
-    TA0CTL = TASSEL_2 + MC_1 + TAIE;             // SMCLK, upmode
+    TA0CTL = TASSEL_2 + MC_1;             // SMCLK, upmode
 
     // start timer
     P2DIR |= 0x1E;       // P2.1,2,3,4 output
     P2OUT &= ~0x0C;
     P2SEL |= 0x12;       // P2.1,4 option select
-    TA1CCR0 = 500;
+    TA1CCR0 = PWM_MAX;
     TA1CCTL1 = OUTMOD_6; // CCR1 toggle/set (P2.1)
     TA1CCR1 = 0;
     TA1CCTL2 = OUTMOD_6; // CCR2 toggle/set (P2.4)
     TA1CCR2 = 0;
 
     TA1CTL = TASSEL_2 + MC_1;             // SMCLK, upmode
-
+	
+	int loc_rxcnt = rxcnt;
+	
     while(1) {
         __bis_SR_register(LPM0_bits + GIE); // Enter LPM0, interrupts enabled
-        int left, right, ch0i, ch1i;
-        bool led = false;
+		
+		if (loc_rxcnt != rxcnt) { // new channel data arrived
+			int16_t left, right;
+			bool led = false;
 
-        ch0i = ch[0]-CENTER;
-        ch1i = ch[1]-CENTER;
-        left  = ch1i + ch0i;
-        right = ch1i - ch0i;
-        if (left>500)   left  =  500;
-        if (left<-500)  left  = -500;
-        if (right>500)  right =  500;
-        if (right<-500) right = -500;
+			xy2lr(ch[0], ch[1], &left, &right);
 
-        if (left>0) {
-            P2OUT &= ~0x04;
-            if (left>PWM_THOLD) {
-                TA1CCR1 = left;
-                led = true;
-            } else
-                TA1CCR1 = 0;
-        }
-        else {
-            P2OUT |= 0x04;
-            if (left<-PWM_THOLD) {
-                TA1CCR1 = 500+left;
-                led = true;
-            } else
-                TA1CCR1 = 501;
-        }
+			if (set_pwm_1(left) || set_pwm_2(right))
+				led = true;
 
-        if (right>0) {
-            P2OUT &= ~0x08;
-            if (right>PWM_THOLD) {
-                TA1CCR2 = right;
-                led = true;
-            } else
-                TA1CCR2 = 0;
-        }
-        else {
-            P2OUT |= 0x08;
-            if (right<-PWM_THOLD) {
-                TA1CCR2 = 500+right;
-                led = true;
-            } else
-                TA1CCR2 = 501;
-        }
-
-        if (led) LED_GREEN_ON();
-        else LED_GREEN_OFF();
+			if (led) LED_GREEN_ON();
+			else LED_GREEN_OFF();
+			
+			TA0CCR1 = ch[2];
+			
+			loc_rxcnt = rxcnt;
+		}
+		else { // no input signal detected
+			TA1CCR1 = 0;
+			TA1CCR2 = 0;
+			P2OUT &= ~0x0C;
+			TA0CCR1 = 0;
+			LED_RED_SWAP();
+		}
     }
     return 1;
 }
@@ -169,12 +210,16 @@ void __attribute__ ((interrupt(WDT_VECTOR))) watchdog_timer (void)
 #endif
 {
     static int rxcnt_test = 0;
-    if (rxcnt_test==rxcnt) {
-        LED_RED_OFF();
-        TA1CCR1 = 0;
-        TA1CCR2 = 0;
-        P2OUT &= ~0x0C;
+	static int cnt = 0;
+    if (rxcnt_test == rxcnt) {
+		if (cnt >= 10) {
+			cnt = 0;
+			__bic_SR_register_on_exit(LPM0_bits);
+		}
+		else cnt++;
     }
+	else cnt = 0;
+
     rxcnt_test = rxcnt;
 }
 
@@ -224,24 +269,4 @@ void __attribute__ ((interrupt(USCIAB0RX_VECTOR))) USCI0RX_ISR (void)
         s=0;
         break;
     }
-}
-
-// Timer_A2 Interrupt Vector (TA0IV) handler
-#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
-#pragma vector=TIMER0_A2_VECTOR
-__interrupt void Timer_A2(void)
-#elif defined(__GNUC__)
-void __attribute__ ((interrupt(TIMER0_A1_VECTOR))) Timer_A2 (void)
-#else
-#error Compiler not supported!
-#endif
-{
-	TA0CCTL0 &= ~0x04;
-  /*switch( TA0IV )
-  {
-  case  2: CCR1 += 1000;                    // Add Offset to CCR1
-           break;
-  case 10: P1OUT ^= 0x01;                   // Timer_A3 overflow
-           break;
- }*/
 }
