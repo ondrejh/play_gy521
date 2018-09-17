a8f9c357e5acec884d5db578e7dc3de3a8c5a666
Ondrej Hejda <ondrejh.ck@email.cz>
2018-09-17 06:50:25 +0200

    i2c init

--- main.c
+++ main.c
@@ -27,13 +27,14 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define LED_INIT() do{P1DIR|=0x41;P1OUT&=~0x41;P1SEL&=~0x41;P1SEL2&=~0x41;}while(0)
+#define LED_INIT() do{P1DIR|=0x01;P1OUT&=~0x01;P1SEL&=~0x01;P1SEL2&=~0x01;}while(0)
+//#define LED_INIT() do{P1DIR|=0x41;P1OUT&=~0x41;P1SEL&=~0x41;P1SEL2&=~0x41;}while(0)
 #define LED_RED_ON() do{P1OUT|=0x01;}while(0)
 #define LED_RED_OFF() do{P1OUT&=~0x01;}while(0)
 #define LED_RED_SWAP() do{P1OUT^=0x01;}while(0)
-#define LED_GREEN_ON() do{P1OUT|=0x40;}while(0)
+/*#define LED_GREEN_ON() do{P1OUT|=0x40;}while(0)
 #define LED_GREEN_OFF() do{P1OUT&=~0x40;}while(0)
-#define LED_GREEN_SWAP() do{P1OUT^=0x40;}while(0)
+#define LED_GREEN_SWAP() do{P1OUT^=0x40;}while(0)*/
 
 int main(void)
 {
@@ -44,6 +45,7 @@ int main(void)
     BCSCTL1 = CALBC1_1MHZ;           // Set DCO
     DCOCTL = CALDCO_1MHZ;
 
+    // init uart
     P1SEL = BIT1 + BIT2 ;            // P1.1 = RXD, P1.2=TXD
     P1SEL2 = BIT1 + BIT2;
     UCA0CTL1 |= UCSSEL_2;            // SMCLK
@@ -52,6 +54,18 @@ int main(void)
     UCA0MCTL = UCBRS2 + UCBRS0;      // Modulation UCBRSx = 5
     UCA0CTL1 &= ~UCSWRST;            // **Initialize USCI state machine**
     IE2 |= UCA0RXIE;                 // Enable USCI_A0 RX interrupt
+
+    // init i2c
+    P1SEL |= BIT6 + BIT7;                     // Assign I2C pins to USCI_B0
+    P1SEL2|= BIT6 + BIT7;                     // Assign I2C pins to USCI_B0
+    UCB0CTL1 |= UCSWRST;                      // Enable SW reset
+    UCB0CTL0 = UCMST+UCMODE_3+UCSYNC;         // I2C Master, synchronous mode
+    UCB0CTL1 = UCSSEL_2+UCSWRST;              // Use SMCLK, keep SW reset
+    UCB0BR0 = 3;                              // fSCL = SMCLK/3 = ~400kHz
+    UCB0BR1 = 0;
+    UCB0I2CSA = 0x68;                         // Set slave address
+    UCB0CTL1 &= ~UCSWRST;                     // Clear SW reset, resume operation
+    IE2 |= UCB0RXIE;                          // Enable RX interrupt
 
     // setup LEDs
     LED_INIT();
